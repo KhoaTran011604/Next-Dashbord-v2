335008a6b810b5ef35edd63de3f16b0c1b200a62
KhoaTran160401 <tranvankhoa.ag.01@gmail.com>
2025-07-17 20:23:59 +0700

    mini update

--- src/app/orders/[id]/page.tsx
+++ src/app/orders/[id]/page.tsx
@@ -24,6 +24,7 @@ import OrderStatus, { PaymentStatus } from "enum/orderEnum";
 import { useQueryClient } from "@tanstack/react-query";
 import useStore from "zustand/store";
 import AddDetailOrder from "@/components/Order/AddDetailOrder";
+import LottieComponent from "@/components/lotties/lottie";
 
 const TYPE_OF_DATA_IMG_RETURN = "file";
 const dataInit = {
@@ -215,180 +216,186 @@ const OrderDetailPage = () => {
         hiddenGoBackBtn={false}
       />
       <div className=" min-h-[calc(100vh-180px)] custom-scrollbar overflow-hidden  rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6">
-        <HyperFormWrapper
-          schema={orderSchema}
-          defaultValues={request}
-          onSubmit={isEdit ? UpdateData : SaveData}
-          className="grid grid-cols-1 md:grid-cols-4 gap-6"
-        >
-          <div className="col-span-1 md:col-span-3">
-            <HD_Input
-              title="Shipping Address"
-              name="shippingAddress"
-              placeholder=""
-              isItemForm={true}
-              initValue={request.shippingAddress}
-              onChange={(value) =>
-                setRequest({
-                  ...request,
-                  shippingAddress: value,
-                })
-              }
-            />
-          </div>
-
-          <div>
-            <Select
-              {...{
-                error: errors.includes("status"),
-                hint: errors.includes("status") ? "Required field" : "",
-              }}
-              title={"Status"}
-              name={"status"}
-              defaultValue={request.status}
-              options={Object.values(OrderStatus).map((val) => ({
-                label: val,
-                value: val,
-              }))}
-              placeholder="Select an option"
-              onChange={(e) => {
-                setRequest({
-                  ...request,
-                  status: e.value,
-                });
-              }}
-              className="dark:bg-dark-900"
-            />
-          </div>
-          <div>
-            <Select
-              {...{
-                error: errors.includes("paymentStatus"),
-                hint: errors.includes("paymentStatus") ? "Required field" : "",
-              }}
-              title={"Payment Status"}
-              name={"paymentStatus"}
-              defaultValue={request.paymentStatus}
-              options={Object.values(PaymentStatus).map((val) => ({
-                label: val,
-                value: val,
-              }))}
-              placeholder="Select an option"
-              onChange={(e) => {
-                setRequest({
-                  ...request,
-                  paymentStatus: e.value,
-                });
-              }}
-              className="dark:bg-dark-900"
-            />
-          </div>
-
-          <div>
-            <HD_Input
-              title="Total"
-              name="totalAmount"
-              type="number"
-              placeholder=""
-              isItemForm={true}
-              initValue={request?.totalAmount?.toString()}
-              onChange={(value) =>
-                setRequest({
-                  ...request,
-                  totalAmount: parseInt(value),
-                })
-              }
-            />
-          </div>
-          <div className="col-span-1 md:col-span-4">
-            <AddDetailOrder
-              isEdit={true}
-              initData={request.items}
-              isConfirm={false}
-              onChange={(response) => {
-                setRequest({
-                  ...request,
-                  items: response,
-                });
-              }}
-            />
-          </div>
-          <div className="col-span-1 md:col-span-4">
-            <HD_TextArea
-              title="Description"
-              name="description"
-              placeholder=""
-              isItemForm={true}
-              initValue={request.description}
-              onChange={(value) =>
-                setRequest({
-                  ...request,
-                  description: value,
-                })
-              }
-            />
-          </div>
-
-          <div className="col-span-1  md:col-span-4">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div className="">
-                <DropzoneComponentV2
-                  title={"Images"}
-                  name={"images-upload"}
-                  multiple={true}
-                  typeDataReturn={TYPE_OF_DATA_IMG_RETURN}
-                  imagesInit={images || []}
-                  onUpload={(dataReturn) => {
-                    setImages(dataReturn);
-                  }}
-                />
-              </div>
-              <div className="mt-4">
-                {images?.length > 0 &&
-                  images.map((itemImg) => (
-                    <div key={Math.random()}>
-                      {(itemImg.imageBase64String != "" ||
-                        itemImg.imageAbsolutePath != "") && (
-                        <div className="flex items-center space-x-4 border border-gray-300 dark:border-gray-700 rounded-lg relative my-4 p-2">
-                          <div className=" w-[100px] h-[100px]  ">
-                            <img
-                              src={
-                                itemImg.isNewUpload
-                                  ? itemImg.imageBase64String
-                                  : itemImg.imageAbsolutePath
-                              }
-                              className="w-full h-full"
-                              style={{ objectFit: "contain" }}
-                            />
-                            <div
-                              className="hover:bg-red-500 absolute top-0 right-0  translate-x-2 -translate-y-2 p-2 bg-gray-800 text-white rounded-lg dark:bg-white dark:text-black"
-                              onClick={() => {
-                                handleDeleteImage(itemImg);
-                              }}
-                            >
-                              <CloseIcon className="size-5 " />
-                            </div>
-                          </div>
-                          <h3 className="text-lg  flex-1 truncate ">
-                            {itemImg.fileName}
-                          </h3>
-                        </div>
-                      )}
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </div>
-          <div className="col-span-1 md:col-span-4 flex justify-end py-2 ">
-            <div>
-              <Button
-                type="submit"
-                children={"Save"}
-                //onClick={isEdit ? UpdateData : SaveData}
+        {isBusy ? (
+          <LottieComponent />
+        ) : (
+          <HyperFormWrapper
+            schema={orderSchema}
+            defaultValues={request}
+            onSubmit={isEdit ? UpdateData : SaveData}
+            className="grid grid-cols-1 md:grid-cols-4 gap-6"
+          >
+            <div className="col-span-1 md:col-span-3">
+              <HD_Input
+                title="Shipping Address"
+                name="shippingAddress"
+                placeholder=""
+                isItemForm={true}
+                initValue={request.shippingAddress}
+                onChange={(value) =>
+                  setRequest({
+                    ...request,
+                    shippingAddress: value,
+                  })
+                }
               />
             </div>
-          </div>
-        </HyperFormWrapper>
+
+            <div>
+              <Select
+                {...{
+                  error: errors.includes("status"),
+                  hint: errors.includes("status") ? "Required field" : "",
+                }}
+                title={"Status"}
+                name={"status"}
+                defaultValue={request.status}
+                options={Object.values(OrderStatus).map((val) => ({
+                  label: val,
+                  value: val,
+                }))}
+                placeholder="Select an option"
+                onChange={(e) => {
+                  setRequest({
+                    ...request,
+                    status: e.value,
+                  });
+                }}
+                className="dark:bg-dark-900"
+              />
+            </div>
+            <div>
+              <Select
+                {...{
+                  error: errors.includes("paymentStatus"),
+                  hint: errors.includes("paymentStatus")
+                    ? "Required field"
+                    : "",
+                }}
+                title={"Payment Status"}
+                name={"paymentStatus"}
+                defaultValue={request.paymentStatus}
+                options={Object.values(PaymentStatus).map((val) => ({
+                  label: val,
+                  value: val,
+                }))}
+                placeholder="Select an option"
+                onChange={(e) => {
+                  setRequest({
+                    ...request,
+                    paymentStatus: e.value,
+                  });
+                }}
+                className="dark:bg-dark-900"
+              />
+            </div>
+
+            <div>
+              <HD_Input
+                title="Total"
+                name="totalAmount"
+                type="number"
+                placeholder=""
+                isItemForm={true}
+                initValue={request?.totalAmount?.toString()}
+                onChange={(value) =>
+                  setRequest({
+                    ...request,
+                    totalAmount: parseInt(value),
+                  })
+                }
+              />
+            </div>
+            <div className="col-span-1 md:col-span-4">
+              <AddDetailOrder
+                isEdit={true}
+                initData={request.items}
+                isConfirm={false}
+                onChange={(response) => {
+                  setRequest({
+                    ...request,
+                    items: response,
+                  });
+                }}
+              />
+            </div>
+            <div className="col-span-1 md:col-span-4">
+              <HD_TextArea
+                title="Description"
+                name="description"
+                placeholder=""
+                isItemForm={true}
+                initValue={request.description}
+                onChange={(value) =>
+                  setRequest({
+                    ...request,
+                    description: value,
+                  })
+                }
+              />
+            </div>
+
+            <div className="col-span-1  md:col-span-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="">
+                  <DropzoneComponentV2
+                    title={"Images"}
+                    name={"images-upload"}
+                    multiple={true}
+                    typeDataReturn={TYPE_OF_DATA_IMG_RETURN}
+                    imagesInit={images || []}
+                    onUpload={(dataReturn) => {
+                      setImages(dataReturn);
+                    }}
+                  />
+                </div>
+                <div className="mt-4">
+                  {images?.length > 0 &&
+                    images.map((itemImg) => (
+                      <div key={Math.random()}>
+                        {(itemImg.imageBase64String != "" ||
+                          itemImg.imageAbsolutePath != "") && (
+                          <div className="flex items-center space-x-4 border border-gray-300 dark:border-gray-700 rounded-lg relative my-4 p-2">
+                            <div className=" w-[100px] h-[100px]  ">
+                              <img
+                                src={
+                                  itemImg.isNewUpload
+                                    ? itemImg.imageBase64String
+                                    : itemImg.imageAbsolutePath
+                                }
+                                className="w-full h-full"
+                                style={{ objectFit: "contain" }}
+                              />
+                              <div
+                                className="hover:bg-red-500 absolute top-0 right-0  translate-x-2 -translate-y-2 p-2 bg-gray-800 text-white rounded-lg dark:bg-white dark:text-black"
+                                onClick={() => {
+                                  handleDeleteImage(itemImg);
+                                }}
+                              >
+                                <CloseIcon className="size-5 " />
+                              </div>
+                            </div>
+                            <h3 className="text-lg  flex-1 truncate ">
+                              {itemImg.fileName}
+                            </h3>
+                          </div>
+                        )}
+                      </div>
+                    ))}
+                </div>
+              </div>
+            </div>
+            <div className="col-span-1 md:col-span-4 flex justify-end py-2 ">
+              <div>
+                <Button
+                  type="submit"
+                  children={"Save"}
+                  //onClick={isEdit ? UpdateData : SaveData}
+                />
+              </div>
+            </div>
+          </HyperFormWrapper>
+        )}
       </div>
     </div>
   );
